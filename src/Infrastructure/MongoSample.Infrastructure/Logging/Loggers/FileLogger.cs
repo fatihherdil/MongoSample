09b167ff9944e6696997c108ba2916ed3b4440f8
Fatih Erdil <mehmett.erdil@gmail.com>
2019-12-19 00:07:56 +0300

    StreamWriter Fixed - JsonSerialize Bug Persists

--- src/Infrastructure/MongoSample.Infrastructure/Logging/Loggers/FileLogger.cs
+++ src/Infrastructure/MongoSample.Infrastructure/Logging/Loggers/FileLogger.cs
@@ -9,6 +9,7 @@ using System.Reflection;
 using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
+using System.Transactions;
 
 namespace MongoSample.Infrastructure.Logging.Loggers
 {
@@ -36,49 +37,49 @@ namespace MongoSample.Infrastructure.Logging.Loggers
 
         void WriteToFile(string filePath, LogStruct log)
         {
+            List<LogStruct> logs = null;
 
-            using (var file = File.Open(filePath, FileMode.OpenOrCreate, FileAccess.ReadWrite))
+            using (var file = File.Open(filePath, FileMode.OpenOrCreate))
             {
-                List<LogStruct> logs = null;
-
                 var readed = string.Empty;
                 using (var reader = new StreamReader(file))
                     readed = reader.ReadToEnd();
 
-                try { logs = JsonConvert.DeserializeObject<List<LogStruct>>(readed); }
-                catch (Exception e) { }
-
-                if (logs == null) logs = new List<LogStruct>();
-
-                logs.Add(log);
-                var logsSerialized = JsonConvert.SerializeObject(logs);
-
-                using (var writer = new StreamWriter(file))
-                {
-                    writer.Write(logsSerialized);
-                    writer.Flush();
-                    writer.Close();
-                }
-                file.Close();
+                logs = JsonConvert.DeserializeObject<List<LogStruct>>(readed);
             }
+
+            if (logs == null) logs = new List<LogStruct>();
+
+            logs.Add(log);
+            var logsSerialized = JsonConvert.SerializeObject(logs);
+
+            using (var writer = File.AppendText(filePath))
+            {
+                writer.Write(logsSerialized);
+                writer.Flush();
+                writer.Close();
+            }
+
         }
 
         [Serializable]
         public class LogStruct
         {
+            [JsonProperty(PropertyName = " @timeStamp")]
+            public string TimeStamp => DateTime.Now.ToString("G");
             public string LogLevel { get; }
             public Event LogEvent { get; }
             public string Formatted { get; }
-            public LogException Exception { get; }
+            public LogException FiredException { get; }
 
             public LogStruct(LogLevel logLevel, EventId eventId, string formatted, Exception exception)
             {
                 LogLevel = logLevel.ToString();
                 LogEvent = new Event(eventId.Id, eventId.Name);
                 Formatted = formatted;
-                Exception = new LogException(exception);
+                FiredException = new LogException(exception);
             }
-
+            [Serializable]
             public class Event
             {
                 public int Id { get; set; }
@@ -90,28 +91,35 @@ namespace MongoSample.Infrastructure.Logging.Loggers
                     Name = name;
                 }
             }
-            public class LogException
+            [Serializable]
+            public sealed class LogException
             {
-                public virtual string Name { get; }
-                public virtual string StackTrace { get; }
-                public virtual string Source { get; set; }
-                public virtual string Message { get; }
-                public Exception InnerException { get; }
-                public int HResult { get; protected set; }
-                public virtual IDictionary Data { get; }
-                //public MethodBase TargetSite { get; }
-                public virtual string HelpLink { get; set; }
+                public string Name { get; }
+                public string StackTrace { get; }
+                public string Source { get; set; }
+                public string Message { get; }
+                public LogException InnerException { get; }
+                public IDictionary Data { get; }
                 public LogException(Exception e)
+                {
+                    Name = e?.GetType()?.Name;
+                    Message = e.Message;
+                    StackTrace = e.StackTrace;
+                    Source = e.Source;
+                    Data = e.Data;
+                    if (e.InnerException != null)
+                        InnerException = new LogException(e.InnerException);
+                }
+                [JsonConstructor]
+                public LogException(LogException e)
                 {
                     Name = e.GetType().Name;
                     Message = e.Message;
                     StackTrace = e.StackTrace;
                     Source = e.Source;
-                    InnerException = e.InnerException;
-                    HResult = e.HResult;
                     Data = e.Data;
-                    //TargetSite = e.TargetSite;
-                    HelpLink = e.HelpLink;
+                    if (e.InnerException != null)
+                        InnerException = new LogException(e.InnerException);
                 }
             }
         }
